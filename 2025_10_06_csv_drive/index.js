4cef515f74d69e8edd20a7deeb0f2f768d5d500a
Uriel Haile <perenganito@gmail.com>
2025-10-06 12:39:50 -0600

    Datos Google

--- 2025_10_06_csv_drive/index.js
+++ 2025_10_06_csv_drive/index.js
@@ -1,3 +1,9 @@
 import {csv} from "d3";
-const data = await csv("./example.csv");
+
+const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR5HdCdLYSmenxp-jkGhGjv-3kkRXjF9qo7npD2oVasAYsSxxGr_r6JT723BVFOBY9r5nisgZEFJ1oL/pub?gid=0&single=true&output=csv";
+// CSV Command Separated Value
+const data = await csv(url);
 console.log(data);
+data.forEach((element)=>{
+  console.log(element)
+});
